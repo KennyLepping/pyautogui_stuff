26a9acb0775457c33cfc1dbacad591b5e52923f2
KennyLepping <kennylepping@gmail.com>
2023-09-06 00:12:29 -0400

    Added shift tab key command remap

--- ableton_remapping.py
+++ ableton_remapping.py
@@ -8,10 +8,14 @@ def main():
             print('Detected ctrl+\\')
             # Press CTRL+E
             pyautogui.hotkey('ctrl', 'e')
-            # Sleep to avoid repeated keypresses
-            keyboard.wait('ctrl')
+            print('Pressed CTRL+E')
+        if keyboard.is_pressed('shift+\\'):
+            print('Detected shift+\\')
+            # Press SHIFT+TAB
+            pyautogui.hotkey('shift', 'tab')
+            print('Pressed SHIFT+TAB')
         # Exit the loop when the user presses 'q'
-        if keyboard.is_pressed('q'):
+        if keyboard.is_pressed('ctrl+q'):
             break
 
 if __name__ == "__main__":
